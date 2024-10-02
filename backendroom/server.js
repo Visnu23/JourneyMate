e44d7d4fd5b0be7a2941196a4596939da2342af7
Visnu Prasanna <166754492+Visnu23@users.noreply.github.com>
2024-10-02 15:03:14 +0530

    Update server.js

--- backendroom/server.js
+++ backendroom/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/jmr', {
+mongoose.connect('process.env.MONGO_URI || mongodb://127.0.0.1:27017/jmr', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
